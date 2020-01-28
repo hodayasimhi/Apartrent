d2ae9a3aeabc93395ed784b6f9eaf93555d1829a
hodayasimhi <hodayasimhii@gmail.com>
2020-01-28 16:45:01 +0200

    make it better

--- Data/User.js
+++ Data/User.js
@@ -2,7 +2,6 @@
 const {Schema,model} = require('mongoose');
 
 const userSchema = new Schema({
-    idUser: {type:String, required:true},
     name: {type:String, required:true},
     userName: {type: String, required:true},
     password: {type: String, required:true},
