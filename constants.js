da0d203070c9d69e57e9e8cff610ea0357ed8509
hodayasimhi <hodayasimhii@gmail.com>
2020-01-28 16:28:56 +0200

    make it better

---
+++ constants.js
@@ -0,0 +1,7 @@
+module.exports = {
+    //DB_HOST : 'mongodb+srv://db_usr:<password>@cluster0-16meh.mongodb.net/test?retryWrites=true&w=majority',
+   //mongodb+srv://db_usr:<password>@cluster0-16meh.mongodb.net/test?retryWrites=true&w=majority
+    DB_HOST : 'cluster0-16meh.mongodb.net/data?retryWrites=true&w=majority',
+    DB_USER : 'db_usr',
+    DB_PASS : 'db_pass',
+};
